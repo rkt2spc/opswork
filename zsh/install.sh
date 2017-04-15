9ffa3516ad4905d95f75bfeedb49f6a3f047d8a1
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-16 06:01:45 +0700

    zsh opswork ensure directories

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,15 +10,20 @@ sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zs
 
 # install awesome-terminal-fonts
 git clone --single-branch -b patching-strategy https://github.com/gabrielelana/awesome-terminal-fonts.git
+mkdir -p ~/.fonts
 cp -af awesome-terminal-fonts/patched/. ~/.fonts
 rm -rf awesome-terminal-fonts
 
 # install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git 
+mkdir -p ~/.oh-my-zsh/custom/themes/powerlevel9k
 cp -af powerlevel9k/. ~/.oh-my-zsh/custom/themes/powerlevel9k
 rm -rf powerlevel9k
 
 # set theme
-git clone https://github.com/rocketspacer/opswork.git
+if [ ! -d opswork ]
+then
+  git clone https://github.com/rocketspacer/opswork.git
+fi
 cp -f opswork/zsh/.zshrc ~/.zshrc
 rm -rf opswork
