ffb8b672a850d02fa8493b14364c5d535af98777
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-16 05:53:15 +0700

    zsh opswork less notifcation on install

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,17 +6,17 @@ sudo apt-get install git
 sudo apt-get install zsh
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install awesome-terminal-fonts
 git clone --single-branch -b patching-strategy https://github.com/gabrielelana/awesome-terminal-fonts.git
-cp -ai awesome-terminal-fonts/patched/. ~/.fonts
-rm -r awesome-terminal-fonts
+cp -af awesome-terminal-fonts/patched/. ~/.fonts
+rm -rf awesome-terminal-fonts
 
 # install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # set theme
 git clone https://github.com/rocketspacer/opswork.git
-cp -i opswork/zsh/.zshrc ~/.zshrc
-rm -r opswork
+cp -f opswork/zsh/.zshrc ~/.zshrc
+rm -rf opswork
